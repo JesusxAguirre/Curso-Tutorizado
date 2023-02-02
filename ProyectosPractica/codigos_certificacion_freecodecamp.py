7fe89a861c306ef7a911aa46c3f82039115b7ad8
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-02 18:29:27 -0400

    creando archivo para estudio de certicicacion en freecode camp

---
+++ ProyectosPractica/codigos_certificacion_freecodecamp.py
@@ -0,0 +1,12 @@
+
+
+width = 15
+height = 12.0
+print(height/3)
+
+words = 'His e-mail is q-lar@freecodecamp.org'
+pieces = words.split()
+parts = pieces[3].split('-')
+n = parts[1]
+print(pieces)
+print(n)
