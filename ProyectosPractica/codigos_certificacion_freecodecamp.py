d76452779a9508383bccc469e4422d866efc95f2
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-03 09:04:13 -0400

    practicando para certifiacion

--- ProyectosPractica/codigos_certificacion_freecodecamp.py
+++ ProyectosPractica/codigos_certificacion_freecodecamp.py
@@ -10,3 +10,34 @@ parts = pieces[3].split('-')
 n = parts[1]
 print(pieces)
 print(n)
+
+counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
+print(counts.get('kris', 0))
+
+
+d = dict()
+d['quincy'] = 1
+d['beau'] = 5
+d['kris'] = 9
+for (clave,valor) in d.items():
+    print(clave, valor)
+
+import re
+s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
+lst = re.findall('\\S+@\\S+', s)
+print(lst)
+
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+mysock.close()
