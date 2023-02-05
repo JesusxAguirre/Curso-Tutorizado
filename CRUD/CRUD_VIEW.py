c9fd101c64ac3b7e2ba4a18e4a543e39fbba88c0
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-05 08:31:02 -0400

    moduralizando aplicacion de crud y mejorando logica de POO

--- CRUD/CRUD_VIEW.py
+++ CRUD/CRUD_VIEW.py
@@ -3,31 +3,35 @@ from tkinter import *
 from modulos_CRUD.clase_conexion import *
 from modulos_CRUD.clase_validaciones import *
 
-root = Tk()
-menu_bar = Menu(root)
-root.config(menu=menu_bar)
-
-
-class Interfaz:
-    def __init__(self, view) -> None:
-        iframe = Frame(root, height=500, width=500)
-
-        iframe.pack()
-
-        iframe_botones = Frame(root)
-        iframe_botones.pack()
-
-        self.view = view
-        self.view.title("Aplicacion con CRUD")
-
+class Interfaz(Frame):
+    def __init__(self, raiz) -> None:
+        #------------variables-----------#
         self.Nombre = StringVar()
         self.Password = StringVar()
         self.Apellido = StringVar()
         self.Direccion = StringVar()
         self.ID = StringVar()
-        # contruyendo los widgets
 
-        # BDD Menu
+        super().__init__(raiz,width=300,height=300)
+        self.master = raiz
+        
+        self.pack()
+        self.master.title("Aplicacion con CRUD")
+        
+        self.iframe_botones = Frame(self)
+        self.iframe_botones.pack()
+        self.crear_barra_menu()
+
+        #self.crear_widgets()
+
+        self.botones_crud()
+       
+        
+
+    def crear_barra_menu(self):
+        menu_bar = Menu(self)
+        self.master.config(menu=menu_bar)
+         # BDD Menu
         self.bd_menu = Menu(menu_bar, tearoff=0)
         self.bd_menu.add_command(
             label="Conectar", command=lambda: objeto_conexion.conectar_bd()
@@ -102,67 +106,72 @@ class Interfaz:
         menu_bar.add_cascade(label="CRUD", menu=self.crud_menu)
         menu_bar.add_cascade(label="Ayuda", menu=self.help_menu)
 
+    def crear_widgets(self):
+        
+    
+        # contruyendo los widgets
         # labels
         # id
-        self.label_id = Label(iframe, text="ID: ", pady=10)
-        self.label_id.grid(row=0, column=0, padx=20)
+        self.label_id = Label(self, text="ID:")
+        self.label_id.grid(row=0, column=0, sticky="e",padx=10, pady=10)
 
-        self.input_id = Entry(iframe, textvariable=self.ID)
+        self.input_id = Entry(self, textvariable=self.ID)
         self.input_id.grid(row=0, column=1)
         # Nombre
         self.label_nombre = Label(
-            iframe,
+            self,
             text="Nombre: ",
             pady=10,
         )
         self.label_nombre.grid(row=1, column=0, padx=20)
 
         self.input_nombre = Entry(
-            iframe, textvariable=self.Nombre, fg="Red", justify=["right"]
+            self, textvariable=self.Nombre, fg="Red", justify=["right"]
         )
         self.input_nombre.grid(row=1, column=1)
 
         # password
-        self.label_password = Label(iframe, text="Password: ", pady=10)
+        self.label_password = Label(self, text="Password: ", pady=10)
         self.label_password.grid(row=2, column=0)
 
         self.input_password = Entry(
-            iframe, textvariable=self.Password, show="*", justify=["left"]
+            self, textvariable=self.Password, show="*", justify=["left"]
         )
         self.input_password.grid(row=2, column=1)
 
         # apellido
-        self.label_apellido = Label(iframe, text="Apellido: ", pady=10)
+        self.label_apellido = Label(self, text="Apellido: ", pady=10)
         self.label_apellido.grid(row=3, column=0)
 
-        self.input_apellido = Entry(iframe, textvariable=self.Apellido)
+        self.input_apellido = Entry(self, textvariable=self.Apellido)
         self.input_apellido.grid(row=3, column=1)
 
         # direccion
-        self.label_direccion = Label(iframe, text="Direccion: ", pady=10)
+        self.label_direccion = Label(self, text="Direccion: ", pady=10)
         self.label_direccion.grid(row=4, column=0)
 
-        self.input_direccion = Entry(iframe, textvariable=self.Direccion)
+        self.input_direccion = Entry(self, textvariable=self.Direccion)
         self.input_direccion.grid(row=4, column=1)
 
         # comentario
-        self.label_comentario = Label(iframe, text="Comentario: ", pady=10)
+        self.label_comentario = Label(self, text="Comentario: ", pady=10)
         self.label_comentario.grid(row=5, column=0)
 
-        self.input_comentario = Text(iframe, width=15, height=4)
+        self.input_comentario = Text(self, width=15, height=4)
         self.input_comentario.grid(row=5, column=1)
 
         # Scroollbar
 
-        self.scrolbar = Scrollbar(iframe, command=self.input_comentario.yview)
+        self.scrolbar = Scrollbar(self, command=self.input_comentario.yview)
         self.scrolbar.grid(row=5, column=2, sticky="nsew")
 
-        # botones
+    def botones_crud(self):
+         # botones
 
         # create
 
         self.btn_create = Button(
-            iframe_botones,
+            self.iframe_botones,
             text="Create",
             command=lambda: objeto_validaciones.validando_formulario(
                 App,
@@ -181,7 +190,7 @@ class Interfaz:
         # read
 
         self.btn_read = Button(
-            iframe_botones,
+            self.iframe_botones,
             text="Read",
             command=lambda: objeto_validaciones.validando_formulario(
                 App,
@@ -200,7 +209,7 @@ class Interfaz:
         # update
 
         self.btn_update = Button(
-            iframe_botones,
+            self.iframe_botones,
             text="Update",
             command=lambda: objeto_validaciones.validando_formulario(
                 App,
@@ -219,7 +228,7 @@ class Interfaz:
         # delete
 
         self.btn_delete = Button(
-            iframe_botones,
+            self.iframe_botones,
             text="Delete",
             command=lambda: objeto_validaciones.validando_formulario(
                 App,
@@ -250,6 +259,8 @@ class Interfaz:
 
     def mostrar_datos(self, datos):
         pass
+
+root = Tk()
 
 
 App = Interfaz(root)
