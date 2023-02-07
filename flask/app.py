0e42ae8f1918f9f1207ac8257e965d405f8ddf09
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-07 07:28:49 -0400

    probando distintas funcionalidades flask

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,22 @@
 from flask import Flask,render_template,url_for
 
+#from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route('/')
 
-def hola_mundo():
+@app.route("/")
+def index():
 
   return render_template("index.html")
+
+@app.route('/hello')
+def hola_mundo():
+
+  return "hello.html"
+
+"""
+@app.route("/username/<username>")
+def show_user_profile(username):
+  return f'User{escape(username)}'
+"""
