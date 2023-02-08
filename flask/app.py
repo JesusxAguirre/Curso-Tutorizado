42a29669f94f671c5e604075bc180ac5fde2b3e6
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-08 14:10:54 -0400

    modificando rutas

--- flask/app.py
+++ flask/app.py
@@ -22,14 +22,14 @@ def get_usuario(nombre_usuario):
 
   return f"Bienvenido a la web {nombre_usuario}"
 
-@app.route("/usuarios_id/<int:id_usuario>")
+@app.route("/usuarios/<int:id_usuario>")
 def get_id_usuario(id_usuario):
 
   #return f"Bienvenido a la web. usuario numero-> {id_usuario}"
   return render_template("usuarios/usuarios.html",id_usuario=id_usuario)
 
 
-@app.route("/datos_usuarios/<int:id>/<string:nombre_usuario>")
+@app.route("/usuarios/<int:id>/<string:nombre_usuario>")
 def get_datos_usuarios(id,nombre_usuario):
   #return f"Estos son los datos del usuario. id-> {id}, nombre-> {nombre_usuario}"
   return render_template("usuarios/datos_usuarios.html",id=id,nombre_usuario=nombre_usuario)
