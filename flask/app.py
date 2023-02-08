b8b5aa163db4758367818497dc1c611ec20dc47b
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-08 10:43:00 -0400

    manejo de rutas con parametros flask

--- flask/app.py
+++ flask/app.py
@@ -1,21 +1,36 @@
 from flask import Flask,render_template,url_for
-
+import os
 #from markupsafe import escape
 
 app = Flask(__name__)
 
-empleados = ["Ana","Maria","Sandra"]
+empleados = ["Ana","Maria","Sandra","Reina"]
 
 
 @app.route("/")
 def index():
 
-  return render_template("index.html")
+  return render_template("index.html",numero_empleados=len(empleados))
 
 @app.route('/hola-mundo')
 def hola_mundo():
 
   return render_template("hello.html")
+
+@app.route("/usuarios/<string:nombre_usuario>")
+def get_usuario(nombre_usuario):
+
+  return f"Bienvenido a la web {nombre_usuario}"
+
+
+
+
+
+if __name__ == "__main__":
+  os.environ['FLASK_DEBUG']= "development"
+  app.run(debug=True)
+
+
 
 """
 @app.route("/username/<username>")
