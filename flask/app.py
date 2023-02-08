348cb548ed8bc884172d18f5336c863927326668
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-08 14:01:55 -0400

    rederizando archivos html pasandole mas de un parametro

--- flask/app.py
+++ flask/app.py
@@ -31,8 +31,8 @@ def get_id_usuario(id_usuario):
 
 @app.route("/datos_usuarios/<int:id>/<string:nombre_usuario>")
 def get_datos_usuarios(id,nombre_usuario):
-
-  return f"Estos son los datos del usuario. id-> {id}, nombre-> {nombre_usuario}"
+  #return f"Estos son los datos del usuario. id-> {id}, nombre-> {nombre_usuario}"
+  return render_template("usuarios/datos_usuarios.html",id=id,nombre_usuario=nombre_usuario)
 
 @app.route("/posts")
 @app.route("/posts/<int:n_post>")
