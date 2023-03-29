91e879d139b85d328d431b0cb6a4662ce4826c88
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-03-29 11:45:43 -0400

    creando formulario de post con validacion y enviando una lista con un diccionario de los post

--- flask/blog_curso/app.py
+++ flask/blog_curso/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, url_for, request, redirect
+from forms import SignupForm,PostForm
 import os
-from forms import SignupForm
-
 
 app = Flask(__name__)
 
+
+app.config['SECRET_KEY'] = "e23895acc525f92e886e9fe425046e0743855fbc038a70067540742c9fd34179"
+
 empleados = ["Ana", "maria", "sandra"]
+
+diccionario_post = []
 
 
 @app.route("/")
-def index():
+def index(diccionario_posts):
 
-    return render_template("index.html", numero_empleados=len(empleados))
+    return render_template("index.html", posts=diccionario_post)
 
 
 @app.route("/quienes")
@@ -23,6 +27,15 @@ def quienes():
 @app.route("/posts")
 @app.route("/posts/<int:npost>")
 def posts(npost):
+    global diccionario_post
+    form = PostForm()
+
+    if form.validate_on_submit():
+        diccionario_post.append(
+            {"titulo": form.titulo.data, "contenido": form.contenido.data})
+
+        redirect(url_for("index",diccionario_post))
+
     return render_template("posts.html", npost=npost)
 
 
@@ -30,15 +43,15 @@ def posts(npost):
 def contacto():
 
     form = SignupForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
 
-        nombre = form.name
-        email = form.email
-        password = form.password
+        nombre = form.name.data
+        email = form.email.data
+        password = form.password.data
 
         return redirect(url_for("index"))
 
-    return render_template("contacto.html")
+    return render_template("contacto.html", form=form)
 
 
 if __name__ == "__main__":
--- flask/blog_curso/forms.py
+++ flask/blog_curso/forms.py
@@ -5,7 +5,32 @@ from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
 
-    name = StringField("nombre", validators=[DataRequired(), Length(max=25)])
-    password = PasswordField("password", validators=[DataRequired(), Length(max=12)])
-    email = StringField("email", validators=[DataRequired(), Email()])
+    name = StringField(
+        "nombre",
+        validators=[DataRequired(),
+                    Length(max=25)]
+    )
+    password = PasswordField(
+        "password",
+        validators=[DataRequired(),
+                    Length(max=12)]
+    )
+    email = StringField(
+        "email",
+        validators=[DataRequired(),
+                    Email()])
     submit = SubmitField("envio_formulario")
+
+
+class PostForm(FlaskForm):
+
+    titulo = StringField(
+        "titulo",
+        validators=[DataRequired(),
+                    Length(max=15)])
+
+    contenido = StringField("contenido",
+                            validators=[DataRequired(),
+                                        Length(max=200)])
+    
+    submit = SubmitField("enviar_post")
